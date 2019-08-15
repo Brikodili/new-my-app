34cb04e669796884a410cf0d904f47b68319aba0
Anna Brikodili <brikodili.troyan@gmail.com>
2019-08-15 23:06:39 +0300

    Update styles

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,7 +3,7 @@ import Layout from 'components/Layout';
 import Article from './article';
 import './style.scss';
 
-export default function() {
+export default function () {
   return (
     <Layout>
       <div className="dashboard has-background-grey-lighter">
@@ -11,15 +11,33 @@ export default function() {
           <div className="columns">
             <aside className="column is-one-third filters">
               <div className="is-flex filters__header">
-                <h5 className="has-text-weight-medium is-size-5">Фильтры</h5>
+                <p className="has-text-weight-medium is-size-5">Фильтры</p>
                 <a href="/" className="has-text-grey-light">Очистить</a>
               </div>
-              <div>
-                <h5 className="has-text-weight-medium is-size-5">Фильтры</h5>
+              <hr/>
+              <div className="filters__body">
+                <p className="result has-text-weight-medium is-size-6">Категории
+                  <span className="has-text-grey-light">(4)</span></p>
+                <div className="filters__input-wrapper">
+                  <input className="filters__input" type="text" placeholder="Название"/>
+                  <img src="https://img.icons8.com/ultraviolet/30/000000/plus.png" alt="Add tag"/>
+                </div>
+                <ul className="is-flex tags">
+                  <li className="tag is-light is-medium">#Landing-page</li>
+                  <li className="tag is-light is-medium">#Логотип</li>
+                  <li className="tag is-light is-medium">#Промо-сайт</li>
+                  <li className="tag is-light">
+                    <img src="https://img.icons8.com/clouds/30/000000/close-window.png" alt="Delete"/>
+                  </li>
+                </ul>
+                <hr/>
+                <div className="filters__valuation">
+                  <p className="result has-text-weight-medium is-size-6">Стоимость</p>
+                </div>
               </div>
             </aside>
             <div className="column">
-              <p className="dashboard__result has-text-weight-medium">Найдено
+              <p className="result has-text-weight-medium">Найдено
                 <span className="has-text-grey-light">(192 услуги)</span></p>
               <div className="box">
                 <Article/>
